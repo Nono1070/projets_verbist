723a652d5a329be68d598634025459d01f37fbf6
Noureddine Meharrar <meharrarn@hotmail.com>
2026-05-06 17:13:06 +0200

    Profil utilisateur et modification d'avatar fonctionnels

--- main/forms.py
+++ main/forms.py
@@ -2,13 +2,16 @@ from django import forms
 from .models import User
 
 class InscriptionForm(forms.ModelForm):
-    # Ajout du champ mot de passe avec masquage des caractères
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirmez le mot de passe")
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'adresse', 'code_postal', 'date_naissance', 'avatar']
+        # AJOUTE CE WIDGET ICI :
+        widgets = {
+            'date_naissance': forms.DateInput(attrs={'type': 'date'}),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
